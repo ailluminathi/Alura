7dbc5f5c6bc9839a527ab7449ad023a4ef74a3d0
Náthali Santos <108452211+ailluminathi@users.noreply.github.com>
2024-08-29 13:57:34 -0300

    att

--- curso-js/explorando-a-linguagem/5-atribuicaoVariaveis.js
+++ curso-js/explorando-a-linguagem/5-atribuicaoVariaveis.js
@@ -1,5 +1,7 @@
 console.log("Trabalhando com atribuição de variáveis");
 
-const idade = 29;
+const idade = 23;
 const nome = "Nath";
- 
+const sobrenome = "Santos"
+
+console.log(nome + " " sobrenome)
